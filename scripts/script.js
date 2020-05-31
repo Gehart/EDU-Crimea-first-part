6f4b53f3adc0da3d4e4ccd0a0f44d5b72ded19fd
Gleb <g.orlovi@ya.ru>
2020-05-31 14:38:27 +0300

    Add owl-carousel, fix header

---
+++ scripts/script.js
@@ -0,0 +1,11 @@
+$(function() {
+  // Owl Carousel
+  var owl = $(".owl-carousel");
+  owl.owlCarousel({
+    items: 4,
+    margin: 30,
+    loop: true,
+    nav: true,    
+    autoWidth:true,
+  });
+});

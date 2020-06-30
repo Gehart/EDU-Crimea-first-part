2787688eb9b11e496af2c0814e9166a42e53b60f
Gleb <g.orlovi@ya.ru>
2020-06-30 12:02:14 +0300

    Вставил стрелочки

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,15 @@
-$('.owl-carousel').owlCarousel({
+var owl = $('.owl-carousel').owlCarousel({
     margin: 30,
     loop: true,
     autoWidth:true,
-    // nav: true,    
+    nav: true,    
 	items: 4,
     dots: true,
-})
+});
+
+$(".next-arrow").click(function(){
+		owl.trigger("next.owl.carousel");
+});
+$(".prev-arrow").click(function(){
+	owl.trigger("prev.owl.carousel");
+});

8bf0c8a5e2d2a909ee9975fd82cca9dc308c62c4
Gleb <g.orlovi@ya.ru>
2020-06-30 18:54:18 +0300

    Доделал слайдер. Адаптивность работает странно

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,25 @@
 var owl = $('.owl-carousel').owlCarousel({
     margin: 30,
+    // stagePadding: 14,	
     loop: true,
-    autoWidth:true,
-    nav: true,    
-	items: 4,
+    responsiveClass:true,
+	navText : ["",""],
     dots: true,
+    dotsEach: true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        900:{
+            items:3
+        },
+        1150:{
+            items:4
+        }
+    }
 });
 
 $(".next-arrow").click(function(){

7e5ebd5b776865784dec7e695efeb238e2a2f5b9
Gleb <g.orlovi@ya.ru>
2020-07-11 16:21:34 +0300

    Добавил сообщения об ошибках

--- scripts/registration.js
+++ scripts/registration.js
@@ -24,10 +24,21 @@ selectBoxs.forEach(o => {
 	dropList(o);
 });
 
-
+// проверка и очистка сообщений об ошибках в полях ввода
 const inputText = document.querySelectorAll(".box-input input");
 inputText.forEach(e=>{
 	e.addEventListener('blur', checkFill);
+	e.addEventListener('focus', removeErrorMessage);
+});
+
+const selected = document.querySelectorAll(".selected");
+selected.forEach(o => {
+	o.addEventListener('click', removeErrorMessage);
+});
+
+const checkboxs = document.querySelectorAll("input[type=checkbox]");
+checkboxs.forEach(o => {
+	o.addEventListener('click', removeErrorMessage);
 });
 
 
@@ -38,4 +49,64 @@ function checkFill() {
 	else {
 		this.classList.remove('is-fill');
 	}
+};
+
+function removeErrorMessage() {
+	this.classList.remove('empty');
+	this.classList.remove('novalid');
+}
+
+
+function validate() {
+	let form = document.forms.form1;
+	let isValidated = true;
+
+	if(!form.subname.value) {
+		form.subname.classList.add("empty");
+		isValidated = false;
+	}
+
+	if(!form.name.value) {
+		form.name.classList.add("empty");
+		isValidated = false;
+	}
+	if(!form.secondName.value) {
+		form.secondName.classList.add("empty");
+		isValidated = false;
+	}
+
+	let emailRegExp = /^[\w]{1}[\w-\.]*@[\w-]+\.[a-z]{2,4}$/i;
+
+	if(!form.email.value){
+		form.email.classList.add("empty");
+		isValidated = false;
+
+	}
+	else if(!emailRegExp.test(form.email.value)) {
+		form.email.classList.add("novalid");
+		isValidated = false;
+
+	}
+
+
+	if(!form.category.value) {
+		document.getElementById('select-category').classList.add('empty');
+		isValidated = false;
+
+	}
+	if(!form.ticket.value) {
+		document.getElementById('select-ticket').classList.add('empty');
+		isValidated = false;
+
+	}
+
+	if(!form.agreement.checked) {
+		form.agreement.classList.add('empty');
+		isValidated = false;
+	}
+
+	if(isValidated)
+		return true;
+	else
+		return false;
 };
